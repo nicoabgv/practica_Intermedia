97ccf91d2affcc6be278fadd9615626aeeba9697
nico <nicoabgv@gmail.com>
2025-05-05 13:48:32 +0200

    Jest configurado para omitir console logs

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 const { server } = require("./index");
 
+global.console = {
+  log: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
+
 afterAll(async () => {
   await mongoose.disconnect();
-  server.close();
+  if (server && server.close) server.close();
 });
